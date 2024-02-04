5f6a19f240c9952d732133bfccc3e635c477ea04
dhruvilmangukiya <mangukiyadhruvil776@gmail.com>
2024-02-04 15:14:14 +0530

    update: Fixed designed issue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 const hbs = require('hbs');
-const app = express();
 const port = process.env.PORT || 8000;
 
 const static_path = path.join(__dirname, "../public");
